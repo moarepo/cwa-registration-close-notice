2db01914f22b77f9a3cfd4fb28e781cdc5d502eb
ChromeDev1 <armani.brown@moa.gov.jm>
2026-09-17 15:51:53 -0500

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+  import { motion, AnimatePresence } from "motion-v";
+</script>
+<template>
+  <AnimatePresence>
+    <motion.section
+      class="flex w-full min-h-svh justify-center items-center p-1 bg-green-500"
+    >
+      <motion.div
+       class="w-full h-[99vh] bg-white p-4 rounded-md border border-dashed flex justify-center items-center"
+      >
+        <h2>Welcome</h2>
+      </motion.div>
+    </motion.section>
+  </AnimatePresence>
+</template>
